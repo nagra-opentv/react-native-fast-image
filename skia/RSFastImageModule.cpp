703896f66db89d82bdc0cd7eda9e92b219ca0913
munezbn.dev <munezbn.dev@gmail.com>
2022-12-12 11:56:06 +0530

    Added RNSkia support
    Except preload, other two APIs implemented

---
+++ skia/RSFastImageModule.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 1994-2022 OpenTV, Inc. and Nagravision S.A.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <cxxreact/JsArgumentHelpers.h>
+
+#include "ReactSkia/utils/RnsLog.h"
+#include "ReactSkia/views/common/RSkImageCacheManager.h"
+
+#include "RSFastImageModule.h"
+
+using namespace folly;
+namespace facebook {
+using namespace react;
+namespace xplat {
+
+RSFastImageModule::RSFastImageModule() { }
+
+auto RSFastImageModule::getConstants() -> std::map<std::string, folly::dynamic> {
+  return {};
+}
+
+std::string RSFastImageModule::getName() {
+  return "FastImageView";
+}
+
+auto RSFastImageModule::getMethods() -> std::vector<Method> {
+  return {
+    Method(
+      "preload",
+      [this](dynamic args) {
+        RNS_LOG_NOT_IMPL;
+        RNS_UNUSED(this);
+        return ;
+      }),
+    Method(
+      "clearMemoryCache",
+      [this] (dynamic args, Callback cb, Callback cbError) {
+        RNS_LOG_NOT_IMPL;
+        RNS_UNUSED(this);
+        RSkImageCacheManager::getImageCacheManagerInstance()->clearMemory();
+        return ;
+      }),
+    Method(
+      "clearDiskCache",
+      [this] (dynamic args, Callback cb, Callback cbError) {
+        RNS_LOG_NOT_IMPL;
+        RSkImageCacheManager::getImageCacheManagerInstance()->clearDisk();
+        RNS_UNUSED(this);
+        return ;
+      }),
+  };
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+RNS_EXPORT_MODULE(RSFastImageModule)
+#ifdef __cplusplus
+}
+#endif
+
+} // namespace xplat
+} // namespace facebook
